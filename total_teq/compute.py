577db58dadc834a2eaa24f17a916ba487b0ad82d
anspu <anspunt@gmail.com>
2016-12-18 22:25:02 +0100

    Changed HTML and compute.

--- total_teq/compute.py
+++ total_teq/compute.py
@@ -5,6 +5,8 @@ Spyder Editor
 This is a temporary script file.
 """
 from scipy.integrate import odeint
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -14,20 +16,18 @@ qf = 0.06
 qc = 0.14
 yy = 0.051
 k = 0.007
-Fabs = 1
+Fabs = 0.885
 e = 0.9
 ey = e*yy
 Vf = 257.6
 Vc = 1840- Vf
 Vegg = 5.76
-dose = 338
-exposure_time = 56
 
 def main(contamination_level,feed_intake,exposure_time):
 
     def GivenDose(t):
         if t <= exposure_time:
-                GivenDose = contamination_level*feed_intake
+                GivenDose = contamination_level*feed_intake*1000
         else:
                 GivenDose = 0
         return GivenDose
@@ -43,10 +43,10 @@ def main(contamination_level,feed_intake,exposure_time):
     GivenDose = (GivenDose,)
     yinit = np.array([0.0,0.0,0.0])
     y = odeint (myModel, yinit, timeGrid, GivenDose)
-    plt.figure() 
+    plt.figure()
     plt.plot(timeGrid, y[:,2]/Vegg) # y[:,0] is the first column of y
     plt.xlabel('time (days)')
-    plt.ylabel('Cegg (pg/g fat)')
+    plt.ylabel('Cegg (pg TEQ/g yolk fat)')
     if not os.path.isdir('static'):
             os.mkdir('static')
     else:
@@ -61,5 +61,3 @@ def main(contamination_level,feed_intake,exposure_time):
 pass
 if __name__ == '__main__':
     int(sys.argv[1], sys.argv[2], sys.argv[3])
-
-    
--- total_teq/models.py
+++ total_teq/models.py
@@ -3,9 +3,9 @@ from django.forms import ModelForm
 
 class Input(models.Model):
     contamination_level = models.FloatField(
-        verbose_name= '(pg/kg feed)', default=0.0)
+        verbose_name= '(ng/kg feed)', default=0.0)
     feed_intake = models.FloatField(
-        verbose_name= '(kg/day)', default=0.0)
+        verbose_name= '(default = 0.116 kg/day)', default=0.116)
     exposure_time = models.FloatField(
         verbose_name= '(days)', default=0.0)
 
